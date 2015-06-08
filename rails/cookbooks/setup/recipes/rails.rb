09f3237886e8cfe679e7d62317454b8b8dcec389
Huei-Horng Yo <hiroshiyui@ecoworkinc.com>
2015-06-08 18:23:28 +0800

    Give the right environment variable HOME
    Signed-off-by: Huei-Horng Yo <hiroshiyui@ecoworkinc.com>

--- rails/cookbooks/setup/recipes/rails.rb
+++ rails/cookbooks/setup/recipes/rails.rb
@@ -2,5 +2,6 @@ Chef::Log.info("Installing Rails, this may take some time...")
 
 execute "Install Rails" do
   user "vagrant"
+  environment ({"HOME" => "/home/vagrant"})
   command "gem install rails"
 end
